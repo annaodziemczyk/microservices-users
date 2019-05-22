f3637b0f07536fb2a4ed37aac42f677527b2f30c
annaodziemczyk <anna.odziemczyk@mycit.ie>
2019-05-22 11:48:03 +0100

    deployment

--- deploy-production.sh
+++ deploy-production.sh
@@ -18,4 +18,8 @@ yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_I
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+kubectl run KUBE_DEPLOYMENT_NAME \
+    --image=deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+    --port=3001
+
+# kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
